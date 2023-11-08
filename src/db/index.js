1a927942013200623c323c57249ce73c6d5e447f
Muhammad Ubaid <ubaid1tahir@gmail.com>
2023-11-09 00:55:54 +0500

    Adding ApiError and ApiResponse Class in utils

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { dbName } from "../constants.js";
+
+const connectionDb = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${dbName}`)
+        console.log("Database connected ", connectionInstance.connection.host)
+    } catch (error) {
+        console.log("Error in connecting to the database ", error);
+        process.exit(1);
+    }
+    
+}
+
+export default connectionDb
---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "Error",
+        errors = [],
+        stackTrace = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.errors = errors
+        this.message = message
+        this.data = null;// to be learn
+        this.success = false
+        if(stack){
+            this.stack = stackTrace
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export default ApiError;
