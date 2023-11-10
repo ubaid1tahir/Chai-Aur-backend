5dc00b18596e146eb72bdf25ea428c10c846d785
Muhammad Ubaid <ubaid1tahir@gmail.com>
2023-11-11 01:51:15 +0500

    made user and video models

---
+++ src/models/user.model.js
@@ -0,0 +1,76 @@
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true,
+        index: true // true if you have to use this field in searching
+    },
+    fullname: { type: String, required: true, trim: true},
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+    coverImage: {
+        type:String,
+    },
+    password: {
+        type:String,
+        required: [true, 'Password is required'],
+    },
+    watchHistory: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'Video'
+        }
+    ],
+    refreshToken: {
+        type: String,
+    }
+
+}, {timestamps: true});
+
+userSchema.pre("save", async function(next){
+    if(!this.modified("password")) return next();
+    this.password = bcrypt(this.password, 10);
+    next();
+})
+
+userSchema.methods.isCorrectPassword = async function(password){
+    return bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        email: this.email,
+        fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    })
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    })
+}
+
+
+export default User = mongoose.model('User',userSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -8,7 +8,7 @@
 //         })
 //     }
 // }
-
+// Dont understand why i use this
 const asyncHandler = (fn) => {
     (req,res,next) => {
         Promise.resolve(fn(req,res,next)).catch(err => next(err));
